1bc33215ba101adf1070fa7fae93df008544697e
Soltanzadeh <f.soltanzadeh.f@gmail.com>
2024-10-11 21:23:50 +0330

    Cleanup: Delete unnecessary imports

--- Main.java
+++ Main.java
@@ -3,22 +3,20 @@
  * 2020-12-11
  */
 
- import java.util.LinkedList;
+import java.util.LinkedList;
 import java.util.List;
-
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
- import javafx.geometry.Pos;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
- import javafx.scene.control.*;
- import javafx.scene.image.Image;
- import javafx.scene.image.ImageView;
- import javafx.scene.layout.*;
- import javafx.stage.Modality;
- import javafx.stage.Stage;
- import javafx.stage.StageStyle;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
  
 
  public class Main extends Application {
